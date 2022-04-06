fee2bcdf9a1f95f13c4051130b4c1eae93452708
emmraw <101661775+emmraw@users.noreply.github.com>
2022-04-06 09:23:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,20 +8,20 @@ import time
 # Welcome to the Cobbles shop app!
 """
 #st.title("Cobbles App")
-st.header('Widgets')
+st.sidebar.header('Widgets')
 
-add_selectbox = st.sidebar.date_input(
+add_selectbox1 = st.sidebar.date_input(
      "Enter the date you wish to predict",
      datetime.date(2020,3,21))
 
-add_selectbox = st.sidebar.selectbox(
+add_selectbox2 = st.sidebar.selectbox(
      "What is today's weather like?",
      ('Sunny', 'Cloudy', 'Rainy'))
 
-add_selectbox = st.sidebar.selectbox(
+add_selectbox3 = st.sidebar.selectbox(
     "Is it a bank holiday?",
     ("No","Yes"))
 
-add_selectbox = st.sidebar.selectbox(
+add_selectbox4 = st.sidebar.selectbox(
     "Is there a special event?",
     ("No","Yes"))
